782b4ade5d4843f04329b5d625782ea1540c74fc
Alberto Rodriguez <ticoc911@gmail.com>
2022-04-20 10:21:10 +0200

    crete routes

---
+++ src/routers/AppRouter.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import { JournalScreen } from '../components/journal/JournalScreen'
+import { AuthRouter } from './AuthRouter'
+
+export const AppRouter = () => {
+  return (
+    <BrowserRouter>
+        
+        <Routes>
+            <Route path='auth/*' element={<AuthRouter/>} />
+
+            <Route path='/' element={<JournalScreen />} />
+        </Routes>
+
+    </BrowserRouter>
+  )
+}
